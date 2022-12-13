41d30d0fc4348d2eb758f80f7d7fb2f88c3cb78e
zakh <zak.hubbard@gmail.com>
2022-12-13 14:15:17 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,41 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+import os
+import openai
+
 import streamlit as st
 
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt="Write a professional cover letter that is two paragraphs long which is for a pre-school teacher named Sarah who is seeking to become an office administrator. Sarah’s skills are project planning, organization, and event facilitation. \n\nDear Hiring Manager,\n\nI am writing to express my interest in the Office Administrator position at your organization. With over 10 years of experience as a Pre-school Teacher, I believe I possess the necessary skills and enthusiasm to excel in this role.\n\nAs a Pre-school Teacher, I have had the opportunity to gain considerable experience in project planning, organization, and event facilitation. These skills have allowed me to take the lead on various projects, successfully plan and execute events, and provide assistance to parents and other members of the school community. I am confident that these skills have laid the groundwork for a successful transition into the Office Administrator role.\n\nI am eager to discuss my qualifications and experience in further detail. Thank you for your time and consideration.\n\nSincerely,\n\nSarah",
+  temperature=0.7,
+  max_tokens=256,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0
+)
+
+
+
 """
-# Welcome to Streamlit!
+# Imperfect Cover Letter
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
 
-In the meantime, below is an example of what you can do with just a few lines of code:
+Generate an imperfect cover letter using the options below:
 """
 
 
 with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+with st.form("my_form"):
+  option = st.selectbox(
+     'Select your current job title:',
+    ('Product Manager', 'Engineer', 'UX Designer', 'Elementary School Teacher', 'Office Administrator', 'Homecare Nurse'))
 
-    points_per_turn = total_points / num_turns
+  option = st.selectbox(
+     'Select your desired job title:',
+    ('Product Manager', 'Engineer', 'UX Designer', 'Elementary School Teacher', 'Office Administrator', 'Homecare Nurse'))
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+  option = st.multiselect('select your relevant skills:', ['writing email', 'event planning', 'writing spreadseets', 'care management', 'lesson planning'])  
+  submitted = st.form_submit_button("Submit")
