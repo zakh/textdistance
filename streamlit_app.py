fd9849807de945ea5629139b8d66bc88699cd241
zakh <zak.hubbard@gmail.com>
2022-12-13 15:11:48 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,15 +34,14 @@ with st.form("my_form"):
    if submitted:
       response = openai.Completion.create(
          model="text-davinci-003",
-         prompt="Write a cover letter which is three paragraphs long. The cover letter is for a person named '" + name + "' who currently has the job title '" + current + "' and who is applying a job with the job title '" + desired + "'. In the cover letter, mention three skills which are shared by people with the job titles '" + current + "' and '" + desired + "' and explain why each skill is relevant to both job titles.",
+         prompt="Write a complete cover letter which is two paragraphs long. The cover letter is for a person named '" + name + "' who currently has the job title '" + current + "' and who is applying a job with the job title '" + desired + "'. In the cover letter, mention three skills which are shared by people with the job titles '" + current + "' and '" + desired + "' and explain why each skill is relevant to both job titles.",
          temperature=0.7,
-         max_tokens=256,
+         max_tokens=512,
          top_p=1,
          frequency_penalty=0,
          presence_penalty=0
       )
-      st.write(response)
-
+      st.write(response.text)
 
 
   
