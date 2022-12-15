b43f90bace407000a4d71ac16acbcca72abb5c28
zakh <zak.hubbard@gmail.com>
2022-12-15 13:37:11 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,12 +29,14 @@ with st.form("my_form"):
    name = st.text_input(label='Enter your name:')
    current = st.text_input(label='Enter your current job title:')
    desired = st.text_input(label='Enter your desired job title:')
-   # Every form must have a submit button.
+   language = st.selectbox(
+    'Language?',
+    ('English', 'French', 'Spanish'), index=0)
    submitted = st.form_submit_button("Submit")
    if submitted:
       response = openai.Completion.create(
          model="text-davinci-003",
-         prompt="Write a complete cover letter which is two paragraphs long and in the french language. The cover letter is for a person named '" + name + "' who currently has the job title '" + current + "' and who is applying a job with the job title '" + desired + "'. In the cover letter, mention three skills which are shared by people with the job titles '" + current + "' and '" + desired + "' and explain why each skill is relevant to both job titles.",
+         prompt="Write a complete cover letter which is two paragraphs long and in the " + language + " language. The cover letter is for a person named '" + name + "' who currently has the job title '" + current + "' and who is applying a job with the job title '" + desired + "'. In the cover letter, mention three skills which are shared by people with the job titles '" + current + "' and '" + desired + "' and explain why each skill is relevant to both job titles.",
          temperature=0.7,
          max_tokens=512,
          top_p=1,
