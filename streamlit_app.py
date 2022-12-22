9f53f1b6c5d5c527f69b7496b0991668bd122e19
zakh <zak.hubbard@gmail.com>
2022-12-22 12:19:51 -0800

    sdfs

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,32 @@
 import os
 import openai
 import streamlit as st
-import streamlit_extras
-from inspect import getmembers, isfunction
-    
-functions_list = getmembers(streamlit_extras)
-st.write(functions_list)
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+def switch_page(page_name: str):
+    from streamlit.runtime.scriptrunner import RerunData, RerunException
+    from streamlit.source_util import get_pages
 
+    def standardize_name(name: str) -> str:
+        return name.lower().replace("_", " ")
+
+    page_name = standardize_name(page_name)
+
+    pages = get_pages("streamlit_app.py")  # OR whatever your main page is called
+
+    for page_hash, config in pages.items():
+        if standardize_name(config["page_name"]) == page_name:
+            raise RerunException(
+                RerunData(
+                    page_script_hash=page_hash,
+                    page_name=page_name,
+                )
+            )
+
+    page_names = [standardize_name(config["page_name"]) for config in pages.values()]
+
+    raise ValueError(f"Could not find page {page_name}. Must be one of {page_names}")
 
 
 
@@ -36,7 +53,7 @@ with st.form("my_form"):
          st.session_state.desired = desired
       if 'language' not in st.session_state:
          st.session_state.language = language
-      ste.switch_page("fssdfs")
+      switch_page("02_page")
 
 
 
