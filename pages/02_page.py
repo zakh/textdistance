5d6074f8eeda98d1dc40e43dac23c335fa20c86d
zakh <zak.hubbard@gmail.com>
2022-12-22 10:27:29 -0800

    page 2 write

--- pages/02_page.py
+++ pages/02_page.py
@@ -1,1 +1,18 @@
+import os
+import openai
 
+import streamlit as st
+from streamlit_extras.switch_page_button import switch_page
+
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+      response = openai.Completion.create(
+         model="text-davinci-003",
+         prompt="Write a complete cover letter and in the " + st.session_state.language + " language. The cover letter is for a person named '" + st.session_state.name + "' who currently has the job title '" + st.session_state.current + "' and who is applying a job with the job title '" + st.session_state.desired + "'. In the cover letter, mention three skills which are shared by people with the job titles '" + st.session_state.current + "' and '" + st.session_state.desired + "' and explain why each skill is relevant to both job titles.",
+         temperature=0.7,
+         max_tokens=512,
+         top_p=1,
+         frequency_penalty=0,
+         presence_penalty=0
+      )
+      st.write(response["choices"][0]["text"])
