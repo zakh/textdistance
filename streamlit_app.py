db2a4c6e537fe0c22ae4ec0b92918b71683e4e43
zakh <zak.hubbard@gmail.com>
2022-12-22 10:40:11 -0800

    button

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ with st.form("my_form"):
          st.session_state.desired = desired
       if 'language' not in st.session_state:
          st.session_state.language = language
-      ste.switch_page("02_page")   
+      ste.switch_page_button.switch_page("02_page")   
 
 
 
