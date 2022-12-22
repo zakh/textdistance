207c8a04acec7a8e2a50907185a1b71525e8073e
zakh <zak.hubbard@gmail.com>
2022-12-22 14:37:41 -0800

    sfd

--- pages/top_skills_for_desired_job.py
+++ pages/top_skills_for_desired_job.py
@@ -9,7 +9,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 response = openai.Completion.create(
    model="text-davinci-003",
-   prompt="Return 13 skills which are related to the job of " + st.session_state.desired + " in the language of " + st.session_state.language + " in the format of a python list. Return only the python list.",
+   prompt="Return 13 skills which are related to the job of " + st.session_state.desired + " in the language of " + st.session_state.language + " in the format of a comma separated list. Return only the list.",
    temperature=0.7,
    max_tokens=512,
    top_p=1,
@@ -17,7 +17,8 @@ response = openai.Completion.create(
    presence_penalty=0
 )
 with st.form('my_form'):
-    st.session_state.skills = st.multiselect('Choose your top job skills for the ' + st.session_state.desired + ' position.', response["choices"][0]["text"])
+    skills = response["choices"][0]["text"].split(",")
+    st.session_state.skills = st.multiselect('Choose your top job skills for the ' + st.session_state.desired + ' position.', skills)
     submitted = st.form_submit_button("Continue")
     if submitted:
         switch_page('strengths')
