12279f26459fec1a4a7eb2c0f8920fb9db9c5609
zakh <zak.hubbard@gmail.com>
2022-12-22 14:18:59 -0800

    fsdf

--- pages/job_in_mind.py
+++ pages/job_in_mind.py
@@ -5,7 +5,7 @@ from switchpage import switch_page
 
 
 with st.form('my_form'):
-    st.session_state.job_in_mind = st.radio('Do you have a specific job in mind?', ('Yes','No'))
+    st.session_state.job_in_mind = st.radio('Do you have a specific job in mind?', ('Yes','No'), index=1)
     if st.session_state.job_in_mind == 'Yes':
         st.session_state.desired =  st.text_input('Enter your desired job title:')
     submitted = st.form_submit_button("Continue")
