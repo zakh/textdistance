450e71ca98b3a43d0d6212c0b9a973fc78aee7ae
zakh <zak.hubbard@gmail.com>
2022-12-22 13:33:26 -0800

    werwer

---
+++ pages/desired_job.py
@@ -0,0 +1,4 @@
+
+   current = st.text_input(label='Enter your current job title:')
+   desired = st.text_input(label='Enter your desired job title:')
+   
--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,38 @@ import streamlit as st
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+
+
+"""
+# Imperfect Cover Letter
+
+
+
+Generate an imperfect cover letter using the options below:
+"""
+
+with st.form("my_form"):
+    name = st.text_input(label='Enter your name:')
+    skin = st.radio(label='Let\'s start with your template. You can always change this later.', ('Contempo', 'Pacific','Whitespace'))
+    language = st.selectbox('Select the language for your cover letter:',('English', 'Mandarin Chinese', 'Hindi', 'Spanish', 'French', 'Standard Arabic', 'Bengali', 'Russian', 'Portuguese', 'Indonesian', 'Urdu', 'Standard German', 'Japanese', 'Swahili', 'Marathi', 'Telugu', 'Western Punjabi', 'Wu Chinese', 'Tamil', 'Turkish', 'Korean', 'Vietnamese', 'Yue Chinese', 'Javanese', 'Italian', 'Egyptian Spoken Arabic', 'Hausa', 'Thai', 'Gujarati', 'Kannada', 'Iranian Persian', 'Bhojpuri', 'Southern Min Chinese', 'Hakka Chinese', 'Jinyu Chinese', 'Filipino', 'Burmese', 'Polish', 'Yoruba', 'Odia', 'Malayalam', 'Xiang Chinese', 'Maithili', 'Ukrainian', 'Moroccan Spoken Arabic', 'Eastern Punjabi', 'Sunda', 'Algerian Spoken Arabic', 'Sundanese Spoken Arabic', 'Nigerian Pidgin', 'Zulu', 'Igbo', 'Amharic', 'Northern Uzbek', 'Sindhi', 'North Levantine Spoken Arabic', 'Nepali', 'Romanian', 'Tagalog', 'Dutch', 'Sa\'idi Spoken Arabic', 'Gan Chinese', 'Northern Pashto', 'Magahi', 'Saraiki', 'Xhosa', 'Malay', 'Khmer', 'Afrikaans', 'Sinhala', 'Somali', 'Chhattisgarhi', 'Cebuano', 'Mesopotamian Spoken Arabic', 'Assamese', 'Northeastern Thai', 'Northern Kurdish', 'Hijazi Spoken Arabic', 'Nigerian Fulfulde', 'Bavarian', 'Bamanankan', 'South Azerbaijani', 'Northern Sotho', 'Setswana', 'Souther Sotho', 'Czech', 'Greek', 'Chittagonian', 'Kazakh', 'Swedish', 'Deccan', 'Hungarian', 'Jula', 'Sadri', 'Kinyarwanda', 'Cameroonian Pidgin', 'Sylheti', 'South Levantine Spoken Arabic', 'Tunisian Spoken Arabic', 'Sanaani Spoken Arabic'), index=0)
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        if 'name' not in st.session_state:
+            st.session_state.name = name
+        if 'skin' not in st.session_state:
+            st.session_state.skin = skin
+        if 'desired' not in st.session_state:
+            st.session_state.desired = desired
+        if 'language' not in st.session_state:
+            st.session_state.language = language
+        switch_page("job_in_mind")
+
+
+
+  
+ 
+
+
 def switch_page(page_name: str):
     from streamlit.runtime.scriptrunner import RerunData, RerunException
     from streamlit.source_util import get_pages
@@ -28,34 +60,3 @@ def switch_page(page_name: str):
 
     raise ValueError(f"Could not find page {page_name}. Must be one of {page_names}")
 
-
-
-"""
-# Imperfect Cover Letter
-
-
-
-Generate an imperfect cover letter using the options below:
-"""
-
-with st.form("my_form"):
-   name = st.text_input(label='Enter your name:')
-   current = st.text_input(label='Enter your current job title:')
-   desired = st.text_input(label='Enter your desired job title:')
-   language = st.selectbox('Select the language for your cover letter:',('English', 'Mandarin Chinese', 'Hindi', 'Spanish', 'French', 'Standard Arabic', 'Bengali', 'Russian', 'Portuguese', 'Indonesian', 'Urdu', 'Standard German', 'Japanese', 'Swahili', 'Marathi', 'Telugu', 'Western Punjabi', 'Wu Chinese', 'Tamil', 'Turkish', 'Korean', 'Vietnamese', 'Yue Chinese', 'Javanese', 'Italian', 'Egyptian Spoken Arabic', 'Hausa', 'Thai', 'Gujarati', 'Kannada', 'Iranian Persian', 'Bhojpuri', 'Southern Min Chinese', 'Hakka Chinese', 'Jinyu Chinese', 'Filipino', 'Burmese', 'Polish', 'Yoruba', 'Odia', 'Malayalam', 'Xiang Chinese', 'Maithili', 'Ukrainian', 'Moroccan Spoken Arabic', 'Eastern Punjabi', 'Sunda', 'Algerian Spoken Arabic', 'Sundanese Spoken Arabic', 'Nigerian Pidgin', 'Zulu', 'Igbo', 'Amharic', 'Northern Uzbek', 'Sindhi', 'North Levantine Spoken Arabic', 'Nepali', 'Romanian', 'Tagalog', 'Dutch', 'Sa\'idi Spoken Arabic', 'Gan Chinese', 'Northern Pashto', 'Magahi', 'Saraiki', 'Xhosa', 'Malay', 'Khmer', 'Afrikaans', 'Sinhala', 'Somali', 'Chhattisgarhi', 'Cebuano', 'Mesopotamian Spoken Arabic', 'Assamese', 'Northeastern Thai', 'Northern Kurdish', 'Hijazi Spoken Arabic', 'Nigerian Fulfulde', 'Bavarian', 'Bamanankan', 'South Azerbaijani', 'Northern Sotho', 'Setswana', 'Souther Sotho', 'Czech', 'Greek', 'Chittagonian', 'Kazakh', 'Swedish', 'Deccan', 'Hungarian', 'Jula', 'Sadri', 'Kinyarwanda', 'Cameroonian Pidgin', 'Sylheti', 'South Levantine Spoken Arabic', 'Tunisian Spoken Arabic', 'Sanaani Spoken Arabic'), index=0)
-   submitted = st.form_submit_button("Submit")
-   if submitted:
-      if 'name' not in st.session_state:
-         st.session_state.name = name
-      if 'current' not in st.session_state:
-         st.session_state.current = current
-      if 'desired' not in st.session_state:
-         st.session_state.desired = desired
-      if 'language' not in st.session_state:
-         st.session_state.language = language
-      switch_page("generate")
-
-
-
-  
- 
