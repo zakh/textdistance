7d89a7de3c6c6d8cd2e7fe17abd88a65986a8305
zakh <zak.hubbard@gmail.com>
2022-12-22 15:21:50 -0800

    werw

--- pages/working_style.py
+++ pages/working_style.py
@@ -2,7 +2,7 @@ import streamlit as st
 from switchpage import switch_page
 
 with st.form('my_form'):
-    st.session_state.working_style = st.radio('Artistic','Enterprising','Investigative','Organized','Practical','Service-Oriented')
+    st.session_state.working_style = st.radio('What\'s your working style?', ['Artistic','Enterprising','Investigative','Organized','Practical','Service-Oriented'])
     submitted = st.form_submit_button("Continue")
     if submitted:
         switch_page('generate')
