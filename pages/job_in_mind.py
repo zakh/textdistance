35566d3751d97ef4680d5e1572d042d15060177d
zakh <zak.hubbard@gmail.com>
2022-12-22 13:52:53 -0800

    sdfs

--- pages/job_in_mind.py
+++ pages/job_in_mind.py
@@ -28,11 +28,11 @@ def switch_page(page_name: str):
 
 with st.form('my_form'):
     st.session_state.job_in_mind = st.radio('Do you have a specific job in mind?', ('Yes','No'))
-    if st.session_state.job_in_mind == 'Yes'
+    if st.session_state.job_in_mind == 'Yes':
         st.session_state.desired =  st.text_input(label='Enter your desired job title:')
     submitted = st.form_submit_button("Continue")
     if submitted:
-        if st.session_state.job_in_mind == 'Yes'
+        if st.session_state.job_in_mind == 'Yes':
             switch_page('top skills for desired job')
         else
             switch_page('strenghts')
