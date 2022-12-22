5a22cef0a38d338df9fc91ca075cf4c22c526dd6
zakh <zak.hubbard@gmail.com>
2022-12-22 15:21:02 -0800

    sdfs

--- pages/explain_gap.py
+++ pages/explain_gap.py
@@ -2,10 +2,8 @@ import streamlit as st
 from switchpage import switch_page
 
 
-
-
 with st.form('my_form'):
-    st.session_state.gap_explanation = st.radio('Impacted by Pandemic','Job Searching','Family / Personal Medical Issues', 'Travel', 'Taking Care of Children', 'Education', 'Relocation', 'Sabbatical')
+    st.session_state.gap_explanation = st.radio('Why were you out of work', ['Impacted by Pandemic','Job Searching','Medical Issues', 'Travel', 'Taking Care of Children', 'Education', 'Relocation', 'Sabbatical'])
     submitted = st.form_submit_button("Continue")
     if submitted:
         switch_page('working style')
