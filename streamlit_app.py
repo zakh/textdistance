2ca3c282093b1a23a0c0ee9dc57ba2aaf074fa00
zakh <zak.hubbard@gmail.com>
2023-03-23 11:29:37 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import textdistance
-import re
 
 
 
@@ -14,13 +12,6 @@ import re
 
 Enter a job title: 
 """
-
-with st.form('my_form'):
-    st.session_state.jobtitle = st.text_input('Enter a job title:')
-    submitted = st.form_submit_button("Search")
-   
-        
-
 
 
   
