87b3e394bbf100a417188023e88413021729da75
zakh <zak.hubbard@gmail.com>
2023-03-23 11:26:40 -0700

    Delete pages directory

--- pages/clear_session.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-for key in st.session_state.keys():
-    del st.session_state[key]
--- pages/working_style.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from switchpage import switch_page
-st.set_page_config(initial_sidebar_state="collapsed")
-
-with st.form('my_form'):
-    st.session_state.working_style = st.radio('What\'s your working style?', ['Artistic','Enterprising','Investigative','Organized','Practical','Service-Oriented'])
-    submitted = st.form_submit_button("Continue")
-    if submitted:
-        switch_page('generate')
